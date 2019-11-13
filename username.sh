c9744fc7af511e97d5dacd2bf28c5aad023e34af
Annika Hettmann <annikamh@DESKTOP-BD858K6.localdomain>
2019-11-12 19:04:22 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,9 +3,9 @@
 # author
 echo "Enter a username: "
 read NAME
-while echo "$NAME" | egrep -v "^[0-9 a-z _]{3,12}$" > /dev/null 2>&1
+while echo "$NAME" | egrep -v "^[a-z] [a-z 0-9 _]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - it must only be between 3 and 12 characters and cannot have any special characters save for the underscore character! And no uppercase letters"
+	echo "You must enter a valid username"
 	echo "Enter a username: "
 	read NAME
 done
